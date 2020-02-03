898d23cf5d361061270a3316f692bd3347b990a6
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-03 22:00:51 +0500

    first code

---
+++ 6. CONCATENATING STING AND OTHER TYPR IN PYTHON.py
@@ -0,0 +1,10 @@
+
+
+first_Name = "Muhammad"
+last_Name = "Ahmed"
+print("Asalam-O-Alaikum, " +first_Name +" "+last_Name+" Wellcomme to our Hotel")
+
+full_Name = "Muhammad Ayyan "
+qualification = "Master in Compuuter"
+cpga = "4.4"
+print(full_Name+ " is " +qualification+ " holding " +cpga+ "CPGA")
